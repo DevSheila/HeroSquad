757e2273b7f31edcd568920baeb57308e3b38e24
sheila <devsheilawambui@gmail.com>
2021-04-21 21:10:03 +0300

    create hero class constructor

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -15,4 +15,19 @@ public class Hero {
 
     private boolean occupied;
     private static ArrayList<Hero> heroes = new ArrayList<Hero>();
+
+    public Hero(String name, int age, String powers, String weakness, int defence, int attack){
+        this.name = name;
+        this.age = age;
+        this.powers=powers;
+        this.weakness= weakness;
+        this.defence =defence;
+        this.attack = attack;
+        String imageUrl = "png";
+        this.imageUrl = imageUrl;
+        heroes.add(this);
+        this.id= heroes.size();
+
+
+    }
 }
